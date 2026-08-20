5c71ef38220ad8bcfd8ef4dc3aed34c3bf4d0f26
Juan Ballesteros <juanras2407@gmail.com>
2026-08-19 22:27:08 -0500

    cambios en card para imagenes

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -16,13 +16,13 @@ export default function ProductCard({
       href={`/${slug}/producto/${product.id}`}
       className="group flex flex-col overflow-hidden rounded-xl border border-beige bg-surface transition-all hover:-translate-y-0.5 hover:shadow-md hover:border-taupe"
     >
-      <div className="aspect-square bg-[#f3eee5] overflow-hidden relative">
+      <div className="aspect-square bg-[#f3eee5] overflow-hidden relative flex items-center justify-center">
         {image ? (
           // eslint-disable-next-line @next/next/no-img-element
           <img
             src={image}
             alt={product.name}
-            className="h-full w-full object-cover transition-transform group-hover:scale-105"
+            className="h-full w-full object-contain p-2 transition-transform group-hover:scale-105"
           />
         ) : (
           <div className="h-full w-full flex items-center justify-center text-4xl text-taupe">
